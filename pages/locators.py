e4cb918fbde64ed241856662fbf46dc9c81b1da8
Kosko <xanaxko@gmail.com>
2023-02-23 03:05:03 +0100

    Adding product page tests. Modified files

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,15 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators():
-    REGISTER_FORM=(By.CLASS_NAME, 'register_form')
+    REGISTER_FORM = (By.CLASS_NAME, 'register_form')
     LOGIN_FORM = (By.CLASS_NAME, 'login_form')
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, 'button.btn-add-to-basket')
+    CHECK_BASKET = (By.CSS_SELECTOR, '.alertinner strong')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, 'p.price_color')
+    PRODUCT_BASKET_PRICE = (By.CSS_SELECTOR, '#messages .alert:nth-child(3) strong')
+
---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def should_be_add_product_to_basket(self):
+        self.should_be_add_basket_form()
+        self.adding_product_to_basket()
+
+    def should_be_add_basket_form(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_BASKET), "Add to basket form is not presented"
+
+    def adding_product_to_basket(self):
+        add_to = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
+        add_to.click()
+
+    def check_product_name(self):
+        text_add_product = self.browser.find_element(*ProductPageLocators.CHECK_BASKET).text
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        assert text_add_product == product_name, "Wrong basket product name"
+
+    def check_product_price(self):
+        basket_price = self.browser.find_element(*ProductPageLocators.PRODUCT_BASKET_PRICE).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        assert basket_price == product_price, "Wrong basket product price"
